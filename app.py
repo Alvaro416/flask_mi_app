38f8845787be89acaf0dcde5971a757fb97d925e
Alvaro416 <Aforosalvaro@gmail.com>
2025-08-07 19:09:21 +0200

    Esta es la ultima modificacion

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''
+    enseña la pag principal de la aplicacion
+    renderiza el template 'index.html' que contiene la estructura
+    '''
     return render_template('index.html')
 
 if __name__ == '__main__':
